bfb759b6c6fd5b9af64ba9a83816488851e977ec
Dani <shaykhodani@gmail.com>
2020-10-25 22:18:17 -0700

    Did more changes

--- src/main/java/Problem1/ValidParentheses.java
+++ src/main/java/Problem1/ValidParentheses.java
@@ -1,7 +1,6 @@
 package Problem1;
 
 public class ValidParentheses {
-
     // Do not change signature (function name, parameters, return type)
     public static boolean isValid(String str) {
         if (str == null) {
